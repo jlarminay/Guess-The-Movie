95300f8b9391ce7422edb6346eb6aeff12231373
Josh <josh@ursacreative.ca>
2024-04-12 23:09:29 -0700

    Try base url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/movies/',
+  base: '',
   plugins: [
     vue({
       template: { transformAssetUrls },
